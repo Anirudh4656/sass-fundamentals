a7369aeed57630b7303bfd1343f2f488ff076b4e
ANIRUDH <anirudh4656@gmail.com>
2023-08-21 13:56:59 +0530

    fundamentals

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style/App.scss";
+const sample = () => {
+  return (
+    <div className='productCard'>
+      <h2>sample</h2>
+      <a href="dd">a</a>
+      </div>
+  )
+}
+
+export default sample
